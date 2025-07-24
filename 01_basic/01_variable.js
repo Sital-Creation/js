5358809f457d8365868891984f6943460366939d
Sital-Creation <138277755+Sital-Creation@users.noreply.github.com>
2025-07-24 16:21:34 +0000

    container config done

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,7 @@
+const accountId=144553
+let accountEmail="Sital@123gmail.com"
+var accountPassword="12345"
+accountCity="jajpur"
+console.log()
+
+console.table([accountId,accountEmail,accountPassword,accountCity])

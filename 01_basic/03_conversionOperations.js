95a2fa62f4da7d8449fe896fbc0bfba6acd0ab4b
Sital-Creation <138277755+Sital-Creation@users.noreply.github.com>
2025-07-30 14:47:55 +0000

    conversion operation

--- 01_basic/03_conversionOperations.js
+++ 01_basic/03_conversionOperations.js
@@ -1,18 +1,18 @@
-let score ="33abc"
+//let score ="33abc"
 
-console.log(typeof score);
-console.log(typeof (score));
+//console.log(typeof score);
+//console.log(typeof (score));
 
-let valueInNumber=Number(score)
-console.log(typeof valueInNumber);
-console.log( valueInNumber); /*if score =33abc then o/p Nan , 
-if score is null and undefined then o/p 0,  if  score =true then o/p 1 ,
-if score ="sital" then o/p =Nan */
+//let valueInNumber=Number(score)
+//console.log(typeof valueInNumber);
+//console.log( valueInNumber); /*if score =33abc then o/p Nan , 
+//if score is null and undefined then o/p 0,  if  score =true then o/p 1 ,
+//if score ="sital" then o/p =Nan */
 //"33" =33
 //"33abc"=Nan
 //true =>1; false =>0
-let booleanIsLoggedIn = Boolean (isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let booleanIsLoggedIn = Boolean (isLoggedIn)
+//console.log(booleanIsLoggedIn);
 //1 =>true; 0=>false
 //""=>false
 //"sital"=>true
@@ -22,3 +22,36 @@ let someNumber =33
 let stringNumber =String(someNumber)
 console.log (stringNumber)
 console.log (typeof stringNumber);
+//****************************Operations*************************/
+let value =3
+let negValue=-value
+console.log(negValue);
+
+//consloe.log(2+2);
+//consloe.log(2-2);
+//consloe.log(2*2);
+//consloe.log(2**3);
+//consloe.log(2/2);
+//consloe.log(2%2);
+let str1 ="Sital"
+let str2 = "hello"
+let str3 = str1+str2
+console.log (str3);
+console.log ("1"+2);//12
+console.log (1+"2");//12
+console.log ("1"+"2");//12
+console.log ("1"+2+2);//122
+console.log (1+2+"2");//32
+console.log((3 +4) * 5 / 3);//11.6666666666
+
+console.log(+true);//1
+console.log(+"");//0
+let num1,num2,num3
+num1=num2=num3=2+2
+let gameCounter=100
+gameCounter++;
+++gameCounter;
+console.log(gameCounter);//101
+
+
+

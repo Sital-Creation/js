1c7da675f735ee8a9a5fc92d126f5641f8dc1192
Sital-Creation <138277755+Sital-Creation@users.noreply.github.com>
2025-07-30 13:50:08 +0000

    Conversion of datatypes

---
+++ 01_basic/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score ="33abc"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber);
+console.log( valueInNumber); /*if score =33abc then o/p Nan , 
+if score is null and undefined then o/p 0,  if  score =true then o/p 1 ,
+if score ="sital" then o/p =Nan */
+//"33" =33
+//"33abc"=Nan
+//true =>1; false =>0
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1 =>true; 0=>false
+//""=>false
+//"sital"=>true
+
+let someNumber =33
+
+let stringNumber =String(someNumber)
+console.log (stringNumber)
+console.log (typeof stringNumber);

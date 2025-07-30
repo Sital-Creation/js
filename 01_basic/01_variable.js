d294bf350435d98018d39ad1a3b66210a4002af4
Sital-Creation <138277755+Sital-Creation@users.noreply.github.com>
2025-07-30 07:10:54 +0000

    datatypes

--- 01_basic/01_variable.js
+++ 01_basic/01_variable.js
@@ -3,5 +3,5 @@ let accountEmail="Sital@123gmail.com"
 var accountPassword="12345"
 accountCity="jajpur"
 console.log()
-
+/* Prefer not to use var because of issue in block scope and functional scope*/
 console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict";//treat all js code as newer version
+
+//alret (3+3) // we are using node.js,not browser
+console.log(3
+    +
+    3)// code readability should be high 
+console.log("Sital") 
+
+let name ="Sital"
+let age =18
+let isLoggedIn = false
+let state ;
+
+//number => 2 to power 53
+//bright
+// string =>""
+//boolean => true/false
+//null => standlone value
+//undefined =>
+//symbol =>unique
+
+//object
+console.log ( typeof age);

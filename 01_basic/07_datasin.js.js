07e64d20a6af5511df459ef6f65de30f60bc0b61
Sital-Creation <138277755+Sital-Creation@users.noreply.github.com>
2025-08-01 16:23:19 +0000

    date

---
+++ 01_basic/07_datasin.js.js
@@ -0,0 +1,28 @@
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleDateString());
+console.log(typeof myDate);
+//let myCreatedDate = new Date (2023 ,0,23)//mon jan23 2023
+console.log(myCreatedDate.toDateString());
+//let myCreatedDate = new Date (2023 ,0,23,5,2)
+console.log(myCreatedDate.toLocalString());//1/23/2023,5:02:00 AM
+let myCreatedDate = new Date ("2023-01-14")//1/14/2023,12:00:00 AM
+let myTimeStamp = Date.now()
+console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+console.log(Math.floor(Date.now()/1000));//second mai date ko convert formula
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth()+1);
+console.log(newDate.gety());
+//`${newDate.getDay()}and the time`
+newDate.toLocaleString('default',{
+    weekday:"long",
+   
+})
+
+
+
+
+
